7eb779d8673e73f001d96d89d89000a227e3e60f
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2023-03-18 19:03:51 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

---
+++ my-folder/problems/fibonacci_number/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    private:
+    int fibo(int n)
+    {
+         if(n==0)
+        {
+            return 0;
+        }
+
+        if(n==1)
+        {
+            return 1;
+        }
+
+        return fibo(n-1)+fibo(n-2); 
+
+    }
+public:
+    int fib(int n) {
+        int ans=fibo(n);
+
+        return ans;
+    }
+};

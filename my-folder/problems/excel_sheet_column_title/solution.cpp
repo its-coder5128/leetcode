ef4d2dd589ab83113f2ea53975def402f3176b96
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2023-08-22 08:06:29 +0000

    Sync LeetCode submission - Excel Sheet Column Title (cpp)

---
+++ my-folder/problems/excel_sheet_column_title/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+
+        string ans="";
+
+        while(columnNumber > 0)
+        {
+            ans.push_back('A' + (columnNumber-1)%26);
+
+            columnNumber = (columnNumber-1)/26;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};

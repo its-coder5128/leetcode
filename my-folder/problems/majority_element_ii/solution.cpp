b0cd324030a8f708876d92687e3b125153705fd3
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2023-10-05 06:52:33 +0000

    Sync LeetCode submission - Majority Element II (cpp)

--- my-folder/problems/majority_element_ii/solution.cpp
+++ my-folder/problems/majority_element_ii/solution.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-
         vector<int> ans;
 
+        int cnt1 = 0;
+        int cnt2 = 0;
         int cand1=0,cand2=0;
-        int cnt1 = 0,cnt2 = 0;
 
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i] == cand1)
+            if(cand1 == nums[i])
                 cnt1++;
-            else if(nums[i] == cand2)
+            else if(cand2 == nums[i])
                 cnt2++;
             else if(cnt1 == 0)
             {
@@ -23,30 +23,27 @@ public:
                 cand2 = nums[i];
                 cnt2++;
             }
-            else
-            {
+            else{
                 cnt1--;
                 cnt2--;
             }
         }
-
         cnt1 = 0;
         cnt2 = 0;
 
-        for(int i =0;i<nums.size();i++)
+        for(int i=0;i<nums.size();i++)
         {
             if(nums[i] == cand1)
                 cnt1++;
             else if(nums[i] == cand2)
                 cnt2++;
         }
-
         if(cnt1 > nums.size()/3)
             ans.push_back(cand1);
         if(cnt2 > nums.size()/3)
             ans.push_back(cand2);
+            
 
         return ans;
-        
     }
 };

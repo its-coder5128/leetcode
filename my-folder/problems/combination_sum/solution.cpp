d62aa871699bb22f9164d00623b0e7d8dbd193ce
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2023-10-08 15:52:20 +0000

    Sync LeetCode submission - Combination Sum (cpp)

---
+++ my-folder/problems/combination_sum/solution.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void solve(vector<int>& arr,int index,int tar,vector<int> &temp,vector<vector<int>> &ans)
+    {
+        if(tar == 0)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        if(tar < 0)
+            return;
+
+        for(int i=index;i<arr.size();i++)
+        {
+            temp.push_back(arr[i]);
+            solve(arr,i,tar-arr[i],temp,ans);
+            temp.pop_back();
+        }
+        
+    }
+
+
+         vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+		// dp vector to store all possible combinations of the target sum
+        vector <vector <vector <int>>> dp(target+1);
+        dp[0]={{}};
+        for(int &i:nums)
+        {
+		// Iterating through all the elements from array nums aka candidates
+            for(int j=i;j<=target;j++)
+            {
+			// Finding all possible ways to achieve sum j from element i
+                for(auto v:dp[j-i])
+                {
+                    v.push_back(i);
+                    dp[j].push_back(v);
+                }
+            }
+        }
+		// Finally, returning our ans \U0001f643
+        return dp[target];
+        
+    }
+};

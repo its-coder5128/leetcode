5f1bb518078eb1d4d7a9ecd917f319d30012659d
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2023-10-13 04:25:47 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (cpp)

--- my-folder/problems/min_cost_climbing_stairs/solution.cpp
+++ my-folder/problems/min_cost_climbing_stairs/solution.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-
-    int solve(vector<int> &cost,int n,vector<int> &dp)
-    {
-        if(n==1)
-            return cost[1];
-        if(n==0)
-            return cost[0];
-        if(dp[n] != -1)
-            return dp[n];
-
-        dp[n] = cost[n]+min(solve(cost,n-1,dp),solve(cost,n-2,dp));
-        return dp[n];    
-    }
     int minCostClimbingStairs(vector<int>& cost) {
 
-        int n=cost.size();
-        //vector<int> dp(n+1);
+        int n = cost.size();
 
-        int prev2=cost[0];
-        int prev1=cost[1];
+        cost.push_back(0);
 
-        for(int i=2;i<n;i++)
+        for(int i = 2;i<n+1;i++)
         {
-            int curr= cost[i] + min(prev1,prev2);
-            prev2=prev1;
-            prev1=curr;
+            cost[i] = min(cost[i]+cost[i-1],cost[i]+cost[i-2]);
         }
 
-        return min(prev1,prev2);
+        return cost[n];
         
     }
 };

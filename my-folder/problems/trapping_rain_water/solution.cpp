b23896804909e76739fe932070579da981c84ed4
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2023-10-16 07:38:40 +0000

    Sync LeetCode submission - Trapping Rain Water (cpp)

--- my-folder/problems/trapping_rain_water/solution.cpp
+++ my-folder/problems/trapping_rain_water/solution.cpp
@@ -1,47 +1,23 @@
 class Solution {
 public:
-    int trap(vector<int>& a) {
-        int n = a.size();
-
-        vector<int> left(n);
-        vector<int> right(n);
-
-        int i = 0;
-        int j = n - 1;
-
-        while(i+1 < a.size() && a[i] < a[i+1])
-        {
-            i++;
+    int trap(vector<int>& A) {
+        int n = A.size();
+        int left=0; int right=n-1;
+        int res=0;
+        int maxleft=0, maxright=0;
+        while(left<=right){
+            if(A[left]<=A[right]){
+                if(A[left]>=maxleft) maxleft=A[left];
+                else res+=maxleft-A[left];
+                left++;
+            }
+            else{
+                if(A[right]>=maxright) maxright= A[right];
+                else res+=maxright-A[right];
+                right--;
+            }
         }
-        while(j - 1 >= 0 && a[j] < a[j-1])
-        {
-            j--;
-        }
-        int x = j;
-
-        int maxiL=INT_MIN;
-        int maxiR=INT_MIN;
-
-        for(int k = i;k<=j;k++)
-        {
-            maxiL = max(maxiL,a[k]);
-            maxiR = max(maxiR,a[x]);
-
-            left[k] = maxiL;
-            right[x] = maxiR;
-            x--;
-        }
-
-        int ans = 0;
-
-        while(i<=j)
-        {
-            ans += min(left[i],right[i]) - a[i];
-
-            i++;
-        }
-
-        return ans;
+        return res;
         
     }
 };

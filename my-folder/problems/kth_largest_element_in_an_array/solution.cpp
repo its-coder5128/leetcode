2b5f33d60d91df01d79f79acba5bb3070704fe09
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2023-10-24 06:11:10 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (cpp)

--- my-folder/problems/kth_largest_element_in_an_array/solution.cpp
+++ my-folder/problems/kth_largest_element_in_an_array/solution.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
+    static bool cmp(int a,int b)
+    {
+        return b<a;
+    }
     int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end(),cmp);
 
-        priority_queue<int,vector<int>,greater<int>> q;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            if(q.size() < k)
-            {
-                q.push(nums[i]);
-            }
-            else{
-                if(q.top() < nums[i])
-                {
-                    q.pop();
-                    q.push(nums[i]);
-                }
-            }
-        }
-
-        return q.top();
-        
+        return nums[k-1];
     }
 };

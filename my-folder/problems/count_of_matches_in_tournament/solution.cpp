c973eb79e5d55b0d1c327c443548e920396a3a82
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2023-12-05 06:53:37 +0000

    Sync LeetCode submission - Count of Matches in Tournament (cpp)

---
+++ my-folder/problems/count_of_matches_in_tournament/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        if(n == 1)
+            return 0;
+        if(n == 2)
+            return 1;
+        
+        if((n & 1) == 0)
+            return n/2 + numberOfMatches(n/2);
+        else
+            return (n-1)/2 + numberOfMatches((n-1)/2 + 1);
+    }
+};

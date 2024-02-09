567cb32e77a387ff80532f3e0c3190bdf8dd6531
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-02-09 13:51:53 +0000

    Sync LeetCode submission - Largest Divisible Subset (cpp)

--- my-folder/problems/largest_divisible_subset/solution.cpp
+++ my-folder/problems/largest_divisible_subset/solution.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector <int> dp(nums.size(), 1);
+
+        sort(nums.begin(),nums.end());
+
+        vector<int> dp(nums.size(),1);
         int maxLen = 1;
-        for (int i=0; i<nums.size(); i++) {
-            for (int j=0; j<i; j++) {
-                if (nums[i] % nums[j] == 0)
-                    dp[i] = max(dp[i], dp[j] + 1);
+
+        for(int i = 1;i<nums.size();i++)
+        {
+            for(int j = 0;j<i;j++)
+            {
+                if(nums[i] % nums[j] == 0)
+                    dp[i] = max(dp[i],dp[j] + 1);
             }
-            maxLen = max(maxLen, dp[i]);
+
+            maxLen = max(maxLen,dp[i]);
         }
-        vector <int> ans;
+
+        vector<int> ans;
         int prev = -1;
-        for (int i=nums.size() - 1; i>=0; i--) {
-            if (maxLen == dp[i] && (prev == -1 || prev % nums[i] == 0)) {
+
+        for(int i = nums.size() - 1;i>=0;i--)
+        {
+            if(dp[i] == maxLen && (prev == -1 || nums[prev]%nums[i] == 0))
+            {
                 ans.emplace_back(nums[i]);
                 maxLen--;
-                prev = nums[i];
+                prev = i;
             }
         }
+
         return ans;
+        
     }
 };

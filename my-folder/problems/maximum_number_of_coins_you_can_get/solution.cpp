321c290093f76e797bf81386f52897e469c51264
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-02-20 14:27:28 +0000

    Sync LeetCode submission - Maximum Number of Coins You Can Get (cpp)

--- my-folder/problems/maximum_number_of_coins_you_can_get/solution.cpp
+++ my-folder/problems/maximum_number_of_coins_you_can_get/solution.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
-        int max = 0;
+
+        sort(piles.begin(),piles.end(),greater<int>());
+
         int n = piles.size();
 
-        for (int i : piles) {
-            if (max < i) max = i;
+        int sum = 0;
+
+        for(int i = 1;i<(n - n/3);i = i+2)
+        {
+            cout<<piles[i]<<" ";
+            sum += piles[i];
         }
-        vector<int> freq(max + 1, 0);
-
-        for (int i : piles) {
-            freq[i]++;
-        }
-
-        int coins = 0;
-        int chance = n / 3;
-
-        int turn = 1;
-        int i = max;
-
-        while (chance != 0) {
-            if (freq[i] > 0) {
-                if (turn == 1) turn = 0;
-                else {
-                    chance--;
-                    turn = 1;
-                    coins += i;
-                }
-                freq[i]--;
-            } else {
-                i--;
-            }
-        }
-
-        return coins;
+        return sum;
     }
 };

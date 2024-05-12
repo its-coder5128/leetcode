6466f1e72111608dca30c62b6a20b26fc8e73d2c
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-12 07:15:48 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- my-folder/problems/two_sum/solution.cpp
+++ my-folder/problems/two_sum/solution.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
 
-        unordered_map<int,list<int>> m;
+        unordered_map<int,int> m;
 
-        for(int i=0;i<nums.size();i++)
+        for(int i = 0;i<n;i++)
         {
-            m[nums[i]].push_back(i);
+            int moreNeeded = target - nums[i];
+
+            if(m.find(moreNeeded) != m.end())
+                return {m[moreNeeded],i};
+                
+            m[nums[i]] = i;
         }
 
-        vector<int> temp;
-        for(int i=0;i<nums.size();i++)
-        {
-            int first = nums[i];
-
-            int second = target - nums[i];
-
-            if( m.count(second) != 0 )
-            {
-                int index=m[second].back();
-                if(index != i || (index == i && m[second].size()>1) )
-                {
-                    temp.push_back(i);
-                    temp.push_back(index);
-                    return temp;
-                }
-            }
-        }
-
-        return temp;
-
-        
+        return {-1,-1};
     }
 };

8032d39512bb027bc5707098c4cda285ef66e31e
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-13 08:29:05 +0000

    Sync LeetCode submission - Merge Sorted Array (cpp)

--- my-folder/problems/merge_sorted_array/solution.cpp
+++ my-folder/problems/merge_sorted_array/solution.cpp
@@ -2,45 +2,26 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        int i=0,j=0,temp;
+        int i = 0;
+        int j = 0;
 
-        while(i<m && j<n)
+        while(i < m && j < n)
         {
-            if(nums1[i]<nums2[j])
-            {
+            if(nums1[i] <= nums2[j])
                 i++;
-            }
-            else if(nums1[i]==nums2[j])
-            {
-                i++;
-                temp=m;
-                for(temp;temp>i;temp--)
+            else{
+                for(int k = m-1 ;k>=i;k--)
                 {
-                    nums1[temp]=nums1[temp-1];
+                    swap(nums1[k],nums1[k+1]);
                 }
-                nums1[i]=nums2[j];
+                nums1[i++] = nums2[j++];
                 m++;
-                i++;
-                j++;
-            }
-            else
-            {
-                temp=m;
-                for(temp;temp>i;temp--)
-                {
-                    nums1[temp]=nums1[temp-1];
-                }
-                nums1[i]=nums2[j];
-                m++;
-                i++;
-                j++;
             }
         }
+
         while(j<n)
         {
-            nums1[i]=nums2[j];
-            i++;
-            j++;
+            nums1[i++] = nums2[j++];
         }
         
     }

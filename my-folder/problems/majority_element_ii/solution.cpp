ac3062550eaa0208e263754c4b841396501cd930
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-13 07:13:36 +0000

    Sync LeetCode submission - Majority Element II (cpp)

--- my-folder/problems/majority_element_ii/solution.cpp
+++ my-folder/problems/majority_element_ii/solution.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-
+        int cand1 = 0;
+        int cand2 = 0;
         int cnt1 = 0;
         int cnt2 = 0;
-        int cand1=0,cand2=0;
+        int n = nums.size();
 
-        for(int i=0;i<nums.size();i++)
+        for(int i = 0;i<n;i++)
         {
-            if(cand1 == nums[i])
+            if(cnt1 == 0 && cand2 != nums[i])
+            {
+                cand1 = nums[i];
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0 && cand1 != nums[i])
+            {
+                cand2 = nums[i];
+                cnt2 = 1;
+            }
+            else if(cand1 == nums[i])
                 cnt1++;
             else if(cand2 == nums[i])
                 cnt2++;
-            else if(cnt1 == 0)
-            {
-                cand1 = nums[i];
-                cnt1++;
-            }
-            else if(cnt2 == 0)
-            {
-                cand2 = nums[i];
-                cnt2++;
-            }
             else{
                 cnt1--;
                 cnt2--;
             }
         }
+
         cnt1 = 0;
         cnt2 = 0;
 
-        for(int i=0;i<nums.size();i++)
+        for(int i = 0;i<n;i++)
         {
-            if(nums[i] == cand1)
-                cnt1++;
-            else if(nums[i] == cand2)
-                cnt2++;
+            if(cand1 == nums[i]) cnt1++;
+            else if(cand2 == nums[i]) cnt2++;
         }
-        if(cnt1 > nums.size()/3)
-            ans.push_back(cand1);
-        if(cnt2 > nums.size()/3)
-            ans.push_back(cand2);
-            
+
+        vector<int> ans;
+
+        if(cnt1 > n/3) ans.push_back(cand1);
+        if(cnt2 > n/3) ans.push_back(cand2);
 
         return ans;
     }

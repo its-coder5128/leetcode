ffa80ce92b8fb2d5b80f87d078fdb70e7014261c
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-13 06:53:26 +0000

    Sync LeetCode submission - Pascal's Triangle (cpp)

--- my-folder/problems/pascal's_triangle/solution.cpp
+++ my-folder/problems/pascal's_triangle/solution.cpp
@@ -4,18 +4,24 @@ public:
 
         vector<vector<int>> ans;
 
-        for(int i=0;i<numRows;i++)
+        ans.push_back({1});
+
+        for(int i = 1;i<numRows;i++)
         {
-            vector<int> temp(i+1,1);
-            for(int j=0;j<i-1;j++)
+            vector<int> temp(i+1,0);
+            for(int j = 0;j<=i;j++)
             {
-                temp[j+1] = ans[i-1][j+1] + ans[i-1][j];
+                int n = ans[i-1].size();
+                
+                if(n-1 >= j)
+                    temp[j] += ans[i-1][j];
+                if(j-1 >= 0)
+                    temp[j] += ans[i-1][j-1];
             }
             ans.push_back(temp);
         }
 
         return ans;
-
         
     }
 };

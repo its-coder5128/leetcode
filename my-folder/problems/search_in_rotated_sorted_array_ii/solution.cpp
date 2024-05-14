3c2d7379509baec02e3dc68c7065d0d5f80462ef
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-14 18:06:16 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (cpp)

--- my-folder/problems/search_in_rotated_sorted_array_ii/solution.cpp
+++ my-folder/problems/search_in_rotated_sorted_array_ii/solution.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
+    bool search(vector<int>& arr, int target) {
+        int s = 0;
+        int e = arr.size() - 1;
 
-        for(int i=0;i<nums.size();i++)
+        while(s<=e)
         {
-            if(nums[i] == target)
+            int m = s + (e-s)/2;
+
+            if(arr[m] == target)
                 return true;
+            
+            if(arr[m] == arr[s] && arr[m] == arr[e])
+            {
+                e--;
+                s++;
+                continue;
+            }
+
+            if(arr[s] <= arr[m])
+            {
+                if(arr[s] <= target && target < arr[m])
+                    e = m - 1;
+                else 
+                    s = m + 1;
+            }
+            else{
+                if(target <= arr[e] && arr[m] < target)
+                    s = m + 1;
+                else
+                    e = m - 1;
+            }
         }
 
         return false;
-        
     }
 };

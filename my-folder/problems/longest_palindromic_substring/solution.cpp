80d2702747eafee1d8a619c8da05d263fd632767
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-17 19:22:07 +0000

    Sync LeetCode submission - Longest Palindromic Substring (cpp)

--- my-folder/problems/longest_palindromic_substring/solution.cpp
+++ my-folder/problems/longest_palindromic_substring/solution.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
-    string paliString(string &str,int i,int j)
+    string solve(string s,int i,int j)
     {
-        int s = i;
-        int e = j;
-
-        while(s>=0 && e<str.size() && str[s] == str[e])
+        int n = s.size();
+        if(j == n)
+            return "";
+        
+        while(i>=0 && j<n)
         {
-            s--;
-            e++;
+            if(s[i] == s[j])
+            {
+                i--;
+                j++;
+            }
+            else
+                break;
         }
-        s++;
-        e--;
-
-        return str.substr(s,e-s+1);
+        return s.substr(i+1,j-i-1);
     }
-    string longestPalindrome(string &s) {
-
+    string longestPalindrome(string s) {
+        int maxi = -1;
         string ans = "";
-        int maxi = 0;
+        int n = s.size();
 
-        for(int i=0;i<s.size();i++)
+        for(int i = 0;i<n;i++)
         {
-            string temp1 = paliString(s,i,i);
-            string temp2 = paliString(s,i,i+1);
-            //cout<<i<<" "<<temp<<" "<<maxi<<" "<<temp.size()<<endl;
+            string str1 = solve(s,i,i);
+            string str2 = solve(s,i,i+1);
 
-            if(maxi<temp1.size())
+            // cout<<str1.size()<<" "<<str2.size()<<" "<<maxi<<endl;
+            int x = str1.size();
+            int y = str2.size();
+            if(x >= y && x > maxi)
             {
-                ans = temp1;
-                //cout<<ans<<endl;
-                maxi = temp1.size();
+                // cout<<str1<<" "<<str2<<" "<<i<<endl;
+                
+                maxi = str1.size();
+                ans = str1;
             }
-            if(maxi<temp2.size())
+            else if(y > x && y > maxi)
             {
-                ans = temp2;
-                //cout<<ans<<endl;
-                maxi = temp2.size();
+                maxi = str2.size();
+                ans = str2;
             }
+
         }
 
         return ans;
-        
     }
 };

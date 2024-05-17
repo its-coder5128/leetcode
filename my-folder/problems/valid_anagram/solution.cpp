bcf7cb6a2db481e3d2a0451e93f1bc37a4797688
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-17 17:33:34 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- my-folder/problems/valid_anagram/solution.cpp
+++ my-folder/problems/valid_anagram/solution.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+
         int n = s.size();
         int m = t.size();
-
+        
         if(n!=m)
             return false;
-        
-        vector<int> arr(26,0);
+
+        vector<int> charSet1(26,0);
+        vector<int> charSet2(26,0);
 
         for(int i = 0;i<n;i++)
         {
-            arr[s[i]-'a']++;
-            arr[t[i]-'a']--;
+            int x = s[i] - 'a';
+            int y = t[i] - 'a';
+
+            charSet1[x]++;
+            charSet2[y]++;
         }
 
         for(int i = 0;i<26;i++)
         {
-            if(arr[i])
+            if(charSet1[i] != charSet2[i])
                 return false;
         }
+
         return true;
+
     }
 };

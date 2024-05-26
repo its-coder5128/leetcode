2460b89cc46f1793722299b704db7b25260d16ef
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-26 08:09:52 +0000

    Sync LeetCode submission - Pow(x, n) (cpp)

--- my-folder/problems/pow(x,_n)/solution.cpp
+++ my-folder/problems/pow(x,_n)/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    double myPower(double x, int n)
+    double solve(double x, int n)
     {
         if(n == 0)
             return 1;
         
-        if(n&1)
-            return x * myPower(x*x,n/2);
+        if(n % 2 != 0)
+            return x*solve(x*x,n/2);
         else
-            return myPower(x*x,n/2);
+            return solve(x*x,n/2);
     }
     double myPow(double x, int n) {
+        
+        double ans = solve(x,abs(n));
 
-        if( n >=0 )
-            return myPower(x,n);
-        else
-            return 1/myPower(x,abs(n));
+        if(n < 0)
+            return 1/ans;
 
+        return ans; 
     }
 };

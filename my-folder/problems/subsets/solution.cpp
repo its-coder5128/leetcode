c178c0f453de3b0a6e563f2d5773d84de6dabc52
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-28 08:38:57 +0000

    Sync LeetCode submission - Subsets (cpp)

--- my-folder/problems/subsets/solution.cpp
+++ my-folder/problems/subsets/solution.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void solve(int index,vector<int>& nums,vector<int>& temp,vector<vector<int>> &ans)
-    {
-        if(index == nums.size())
-        {
-            ans.push_back(temp);
-            return;
-        }
-
-        solve(index+1,nums,temp,ans);
-
-        temp.push_back(nums[index]);
-        solve(index+1,nums,temp,ans);
-        temp.pop_back();
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int x = (1<<(n)) - 1;
+        int cnt = 0;
 
         vector<vector<int>> ans;
-        vector<int> temp;
 
-        solve(0,nums,temp,ans);
+        // cout<<x;
+        while(cnt <= x)
+        {
+            vector<int> temp;
+
+            for(int i = 0;(1<<i) <= cnt ;i++)
+            {
+                if(cnt & (1<<i))
+                    temp.push_back(nums[i]);
+            }
+            cnt++;
+            ans.push_back(temp);
+        }
 
         return ans;
         

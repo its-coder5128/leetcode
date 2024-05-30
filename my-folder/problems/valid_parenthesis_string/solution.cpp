8d07152557182dfc4382cd679b5c5ce8bc0f35cf
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-30 08:05:51 +0000

    Sync LeetCode submission - Valid Parenthesis String (cpp)

---
+++ my-folder/problems/valid_parenthesis_string/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mini = 0;
+        int maxi = 0;
+
+        for(auto c:s)
+        {
+            if(c == '(')
+            {
+                mini++;
+                maxi++;
+            }
+            else if(c == ')')
+            {
+                mini--;
+                maxi--;
+            }
+            else
+            {
+                mini--;
+                maxi++;
+            }
+
+            if(mini<0)
+                mini = 0;
+            
+            if(maxi < 0)
+                return false;
+        }
+
+        return mini == 0;
+    }
+};

893ebc0cc26caeab318409999f7900a5e63845dc
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-05-31 07:41:09 +0000

    Sync LeetCode submission - Maximum Profit in Job Scheduling (cpp)

--- my-folder/problems/maximum_profit_in_job_scheduling/solution.cpp
+++ my-folder/problems/maximum_profit_in_job_scheduling/solution.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = profit.size();
 
-        vector<vector<int>> jobs;
+        int size = 0;
 
-        for(int i=0;i<profit.size();i++)
+        vector<vector<int>> jobs(n,vector<int>(3,0));
+
+        for(int i = 0;i<n;i++)
         {
-            jobs.push_back({endTime[i],startTime[i],profit[i]});
+            jobs[i][0] = endTime[i];
+            jobs[i][1] = startTime[i];
+            jobs[i][2] = profit[i];
         }
 
         sort(jobs.begin(),jobs.end());
-        
+
         vector<int> end;
         vector<int> prof;
+
         end.push_back(0);
         prof.push_back(0);
 
-        for(auto num:jobs)
+        for(int i = 0;i<n;i++)
         {
-            int index = upper_bound(end.begin(),end.end(),num[1]) - end.begin();
+            int index = upper_bound(end.begin(),end.end(),jobs[i][1]) - end.begin();
 
             if(index == end.size())
             {
-                end.push_back(num[0]);
-                prof.push_back(prof[prof.size()-1] + num[2]);
-            }else{
-                if(num[2] + prof[index-1] > prof[prof.size()-1])
+                end.push_back(jobs[i][0]);
+                prof.push_back(jobs[i][2] + prof.back());
+            }
+            else
+            {
+                if(prof.back() < jobs[i][2] + prof[index-1])
                 {
-                    end.push_back(num[0]);
-                    prof.push_back(prof[index-1] + num[2]);
+                    end.push_back(jobs[i][0]);
+                    prof.push_back(jobs[i][2] + prof[index-1]);
                 }
             }
         }
 
-        return prof[prof.size()-1];
-        
+        return prof.back();
         
     }
 };

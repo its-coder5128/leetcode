23f646698a5d842f5feacfb89b1b9ea7f4ee21da
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-01 07:49:12 +0000

    Sync LeetCode submission - Rotting Oranges (cpp)

--- my-folder/problems/rotting_oranges/solution.cpp
+++ my-folder/problems/rotting_oranges/solution.cpp
@@ -1,53 +1,57 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int cnt = 0;
+        
         queue<pair<int,int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> row{-1,0,1,0};
+        vector<int> col{0,1,0,-1};
+        int cnt = 0;
+        int time = 0;
 
-        for(int i=0;i<grid.size();i++)
+        for(int i = 0;i<n;i++)
         {
-            for(int j=0;j<grid[0].size();j++)
+            for(int j = 0;j<m;j++)
             {
-                if(grid[i][j] == 1)
-                    cnt++;
-                else if(grid[i][j] == 2)
+                if(grid[i][j] == 2)
                     q.push({i,j});
+                else if(grid[i][j] == 1)
+                {
+                    cnt++;
+                }
             }
         }
-        vector<int> row={-1,0,1,0};
-        vector<int> col={0,1,0,-1};
-        int n=q.size();
-        int ans = 0;
+
         while(!q.empty())
         {
-            auto temp = q.front();
-            q.pop();
+            int qs = q.size();
 
-            if(n-- == 0)
+            for(int i= 0;i<qs;i++)
             {
-                ans++;
-                n=q.size();
-            }
-            
-
-            for(int i=0;i<4;i++)
-            {
-                int r = temp.first + row[i];
-                int c = temp.second + col[i];
-
-                if(r>=0 && r<grid.size() &&  c>=0 && c<grid[0].size() && grid[r][c] == 1)
-                {
-                    grid[r][c] = 2;
-                    cnt--;
-                    q.push({r,c});
-                }
+                auto front = q.front();
+                q.pop();
                 
+                for(int j = 0;j<4;j++)
+                {
+                    int newRow = front.first + row[j];
+                    int newCol = front.second + col[j];
+                    if(newRow >= 0 && newCol >= 0 && newRow < n && newCol < m && grid[newRow][newCol] == 1)
+                    {
+                        grid[newRow][newCol] = 2;
+                        q.push({newRow,newCol});
+                    }
+
+                }
             }
+            if(q.empty())
+                break;
+            cnt -= q.size();
+            time++;
         }
 
-        if(cnt)
-            return -1;
-        
-        return ans;
+
+
+        return cnt == 0 ? time : -1;
     }
 };

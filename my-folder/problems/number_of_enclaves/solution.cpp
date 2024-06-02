cf0cf07a992bb7a0a439982206984a48037ece42
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-02 08:23:28 +0000

    Sync LeetCode submission - Number of Enclaves (cpp)

--- my-folder/problems/number_of_enclaves/solution.cpp
+++ my-folder/problems/number_of_enclaves/solution.cpp
@@ -1,19 +1,51 @@
 class Solution {
 public:
-    int numEnclaves(vector<vector<int>>& A, int res = 0) {
-  queue<pair<int, int>> q;
-  for (auto i = 0; i < A.size(); ++i)
-    for (auto j = 0; j < A[0].size(); ++j) {
-      res += A[i][j];
-      if (i * j == 0 || i == A.size() - 1 || j == A[i].size() - 1) q.push({ i, j });
+    int numEnclaves(vector<vector<int>>& grid) {
+        queue<pair<int,int>> q; 
+        int n = grid.size(); 
+        int m = grid[0].size(); 
+        int cnt = 0;
+        vector<vector<int>> vis = grid; 
+        // traverse boundary elements
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                // first row, first col, last row, last col 
+                cnt += vis[i][j];
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    // if it is a land then store it in queue
+                    if(grid[i][j] == 1) {
+                        q.push({i, j}); 
+                        cnt--;
+                        vis[i][j] = 0; 
+                    }
+                }
+            }
+        }
+        
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, +0, -1}; 
+        
+        while(!q.empty()) {
+            int row = q.front().first; 
+            int col = q.front().second; 
+            q.pop(); 
+            
+            // traverses all 4 directions
+            for(int i = 0;i<4;i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i]; 
+                // check for valid coordinates and for land cell
+                if(nrow >=0 && nrow <n && ncol >=0 && ncol < m 
+                && vis[nrow][ncol] == 1 ) {
+                    q.push({nrow, ncol});
+                    cnt--;
+                    vis[nrow][ncol] = 0; 
+                }
+            }
+            
+        }
+        
+
+        return cnt; 
     }
-  while (!q.empty()) {
-    auto x = q.front().first, y = q.front().second; q.pop();
-    if (x < 0 || y < 0 || x == A.size() || y == A[x].size() || A[x][y] != 1) continue;
-    A[x][y] = 0;
-    --res;
-    q.push({ x + 1, y }), q.push({ x - 1, y }), q.push({ x, y + 1 }), q.push({ x, y - 1 });
-  }
-  return res;
-}
 };

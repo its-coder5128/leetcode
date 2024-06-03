0d45b657f2f5801196b42342feabb83ee6e3205d
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-03 07:45:03 +0000

    Sync LeetCode submission - Find Eventual Safe States (cpp)

--- my-folder/problems/find_eventual_safe_states/solution.cpp
+++ my-folder/problems/find_eventual_safe_states/solution.cpp
@@ -1,60 +1,41 @@
 class Solution {
 public:
-    bool dfsCycle(int node,unordered_map<int, bool> &visited,unordered_map<int, bool> &dfsVisited,
-    vector<vector<int>>& graph,vector<int> &isSafe)
-    {
-        visited[node]=true;
-        dfsVisited[node]=true;
-
-        for(auto nb:graph[node])
-        {
-            if(!visited[nb])
-            {
-                bool cycle=dfsCycle(nb,visited,dfsVisited,graph,isSafe);
-
-                if(cycle)
-                {
-                    isSafe[node]=0;
-                    return true;
-                }
-            }
-            else if(dfsVisited[nb])
-            {
-                isSafe[node]=0;
-                isSafe[nb]=0;
-                return true;
-            }
-        }
-
-        dfsVisited[node]=false;
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-
-        vector<int> ans;
-        unordered_map<int, bool> visited;
-        unordered_map<int, bool> dfsVisited;
-
-
-        int n=graph.size();
-        vector<int> isSafe(n,1);
-
-        for(int i=0;i<n;i++)
+        int n = graph.size();
+        vector<int> adj[n];
+        vector<int> indegree(n,0);
+        for(int i = 0;i<n;i++)
         {
-            if(!visited[i])
+            for(auto it:graph[i])
             {
-                bool isCycle=dfsCycle(i,visited,dfsVisited,graph,isSafe);
+                adj[it].push_back(i);
+                indegree[i]++;
             }
-
         }
 
-        for(int i=0;i<n;i++)
+        queue<int> q;
+        vector<int> ans;
+
+        for(int i = 0;i<n;i++)
+            if(indegree[i] == 0)    
+                q.push(i);
+            
+        while(!q.empty())
         {
-            if(isSafe[i] == 1)
-                ans.push_back(i);
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it] == 0)
+                    q.push(it);
+            }
         }
 
+        sort(ans.begin(),ans.end());
         return ans;
-        
+
     }
 };

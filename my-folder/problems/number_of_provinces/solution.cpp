a1ab27647437e3bbb468e60453b43ab2a91845ec
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-07 12:22:47 +0000

    Sync LeetCode submission - Number of Provinces (cpp)

--- my-folder/problems/number_of_provinces/solution.cpp
+++ my-folder/problems/number_of_provinces/solution.cpp
@@ -1,38 +1,75 @@
 class Solution {
+    class disJoinSet
+    {
+        vector<int> size;
+        vector<int> parent;
+        int comp;
+        
+        public:
+        disJoinSet(int n)
+        {
+            size.resize(n+1);
+            parent.resize(n+1);
+            comp = n;
+            
+            for(int i = 0;i<=n;i++)
+            {
+                size[i] = 1;
+                parent[i] = i;
+            }
+        }
+        
+        int findParent(int x)
+        {
+            if(parent[x] == x)
+                return x;
+            
+            return parent[x] = findParent(parent[x]);
+        }
+        
+        void unionBySize(int x,int y)
+        {
+            int par_x = findParent(x);
+            int par_y = findParent(y);
+            
+            if(par_x == par_y)
+                return;
+            
+            comp--;
+
+            if(size[par_x] > size[par_y])
+            {
+                parent[par_y] = par_x;
+                size[par_x] += size[par_y];
+            }
+            else
+            {
+                parent[par_x] = par_y;
+                size[par_y] += size[par_x];
+            }
+            return;
+        }
+
+        int findComp()
+        {
+            return comp;
+        }
+    };
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
 
-        vector<int> vis(n,0);
-
-        queue<int> q;
-
-        int ans = 0;
+        disJoinSet DJS(n);
 
         for(int i = 0;i<n;i++)
         {
-            if(!vis[i])
+            for(int j = 0;j<n;j++)
             {
-                vis[i] = 1;
-                q.push(i);
-                ans++;
-                while(!q.empty())
-                {
-                    int front = q.front();
-                    q.pop();
-                    for(int j = 0;j<n;j++)
-                    {
-                        if(i != j && !vis[j] && isConnected[front][j])
-                        {
-                            vis[j] = 1;
-                            q.push(j);
-                        }
-
-                    }
-                }
-
+                if(isConnected[i][j] == 1)
+                    DJS.unionBySize(i,j);
             }
         }
-        return ans;
+        
+        return DJS.findComp();
     }
 };

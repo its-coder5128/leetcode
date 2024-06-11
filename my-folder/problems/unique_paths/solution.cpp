d1d4339668e36788a4d555b3a043a2401197cfd0
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-11 10:49:34 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- my-folder/problems/unique_paths/solution.cpp
+++ my-folder/problems/unique_paths/solution.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        vector<int> prev(n,1);
 
-        vector<int> next(n+1,0);
-        vector<int> curr(n+1,0);
-        curr[n-1] = 1;
-
-        swap(m,n);
-       
-
-        for(int i=n-1;i>=0;i--)
+        for(int i = 1;i<m;i++)
         {
-            for(int j=m-1;j>=0;j--)
+            vector<int> curr(n,1);
+            for(int i = 1;i<n;i++)
             {
-                if(i == n-1 && j == m-1)
-                    continue;
-                int down = next[j];
-                int right = curr[j+1];
-                curr[j] = down + right;
+                curr[i] = curr[i-1] + prev[i];
             }
-            next = curr;
+            prev = curr;
         }
 
-        return curr[0];
+        return prev[n-1];
     }
 };

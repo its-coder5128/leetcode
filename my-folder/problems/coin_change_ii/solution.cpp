c48f4b01cacac6628c05bc3dcc14a57984141922
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-13 06:13:53 +0000

    Sync LeetCode submission - Coin Change II (cpp)

--- my-folder/problems/coin_change_ii/solution.cpp
+++ my-folder/problems/coin_change_ii/solution.cpp
@@ -1,60 +1,25 @@
 class Solution {
 public:
-    int solve(int amount, vector<int>& coins,int index,vector<vector<int>> &dp)
-    {
-        if(index == coins.size())
-            return 0;
-        if(amount == 0)
-            return 1;
-        if(amount < 0)
-            return 0;
-        if(dp[index][amount] != -1)
-            return dp[index][amount];
-        
-        int include = solve(amount - coins[index],coins,index,dp);
-        int exclude = solve(amount,coins,index+1,dp);
-
-        return dp[index][amount] = include + exclude;
-    }
-
-    int solveTab(int tar, vector<int>& coins)
-    {
-        //vector<vector<int>> dp(coins.size()+1,vector<int>(tar+1,0));
-
-        vector<int> curr(tar+1,0);
-        vector<int> next(tar+1,0);
-
-        // for(int i=0;i<=coins.size();i++)
-        // {
-        //     dp[i][0] = 1;
-        // }
-        curr[0]=1;
-        next[0]=1;
-
-        for(int index = coins.size()-1;index >= 0;index--)
-        {
-            for(int amount = 1;amount <=tar;amount++)
-            {
-                int include = 0;
-                if(amount - coins[index]>=0)
-                    include = curr[amount - coins[index]];
-                int exclude = next[amount];
-
-                curr[amount] = include + exclude;
-            }
-
-            next = curr;
-        }
-
-        return next[tar];
-    }
     int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(2,vector<int>(amount+1,0));
 
-        // vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+        for(int i= 0;i<=amount;i++)
+            if(i%coins[0] == 0) dp[0][i] = 1;
 
-        // return solve(amount,coins,0,dp);
-
-        return solveTab(amount,coins);
-        
+        for(int i = 1;i<n;i++)
+        {
+            for(int j = 0;j<=amount;j++)
+            {
+                int notTake = dp[0][j];
+                int take = 0;
+                if(j >= coins[i])
+                    take = dp[1][j-coins[i]];
+                
+                dp[1][j] = take + notTake;
+            }
+            dp[0] = dp[1];
+        }
+        return  dp[0][amount];
     }
 };

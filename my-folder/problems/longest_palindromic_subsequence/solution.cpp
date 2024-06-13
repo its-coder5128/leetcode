60138a899d42412ca0abcab2918565499423716b
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-13 11:23:24 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (cpp)

--- my-folder/problems/longest_palindromic_subsequence/solution.cpp
+++ my-folder/problems/longest_palindromic_subsequence/solution.cpp
@@ -1,66 +1,32 @@
 class Solution {
 public:
-    int solve(string s,int i,int j,vector<vector<int>> &dp)
+    int solve(string& s,int i,int j,vector<vector<int>>& dp)
     {
-        if(i == j)
-            return 1;
-        if( i > j)
-            return 0;
-        
+        if(j <= i )
+            return i==j;
         if(dp[i][j] != -1)
-            return dp[i][j];
+            return dp[i][j]; 
         
-        int ans = 0;
-
         if(s[i] == s[j])
-            ans = 2 + solve(s,i+1,j-1,dp);
-        else
-            ans = max(solve(s,i+1,j,dp),solve(s,i,j-1,dp));
-
-        dp[i][j] = ans;
-
-        return ans;
-
-    }
-    int solveTab(string s)
-    {
-       //vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,0)); 
-       vector<int> curr(s.size()+1,0);
-       vector<int> next(s.size()+1,0);
-       
-       for(int i=s.size()-1;i>=0;i--)
-       {
-            for(int j=i;j<s.size();j++)
-           {
-                if(i == j)
-                    curr[j] = 1;
-                else{
-                    int ans = 0;
-
-                    if(s[i] == s[j])
-                        ans = 2 + next[j-1];
-                    else
-                        ans = max(next[j],curr[j-1]);
-
-                    curr[j] = ans;
-                }    
-           }
-
-           next = curr;
-       }
-
-       return next[s.size()-1];
+            return dp[i][j] = 2 + solve(s,i+1,j-1,dp);
+        else{
+            return dp[i][j] = max(solve(s,i+1,j,dp),solve(s,i,j-1,dp));
+        }
+            
     }
     int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i = 0;i<n;i++)  dp[i][i] = 1;
 
-        // vector<vector<int>> dp(s.size(),vector<int>(s.size(),-1));
-
-        // return solve(s,0,s.size()-1,dp);
-
-        if(s.size()<2)
-            return s.size();
-
-        return solveTab(s);
-        
+        for(int i = n-1;i>=0;i--)
+        {
+            for(int j = i+1;j<n;j++)
+            {
+                if(s[i] == s[j]) dp[i][j] = 2 + dp[i+1][j-1];
+                else dp[i][j] = max(dp[i+1][j],dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
     }
 };

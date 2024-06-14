d46437165cef2a58c8d900430e82e21a0b6e068f
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-14 08:53:39 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock IV (cpp)

--- my-folder/problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
+++ my-folder/problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
@@ -1,77 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int>& prices,int index,int buy,int k,vector<vector<vector<int> > > &dp)
-    {
-        if(index == prices.size() || k == 0)
-        {
-            return 0;
-        }
-        if(dp[index][buy][k] != -1)
-            return dp[index][buy][k];
-        
-        int profit = 0;
-
-        if(buy)
-        {
-            int take = -prices[index] + solve(prices,index + 1,0,k,dp);
-            int notTake = solve(prices,index + 1,1,k,dp);
-
-            profit = max(take,notTake);
-        }
-        else{
-            int take = prices[index] + solve(prices,index + 1,1,k-1,dp);
-            int notTake = solve(prices,index + 1,0,k,dp);
-
-            profit = max(take,notTake);
-
-        }
-
-        dp[index][buy][k] = profit;
-        return profit;
-    }
-    int solveTab(int limit, vector<int>& prices)
-    {
-        //vector<vector<vector<int> > > dp(prices.size() + 1,vector<vector<int>>(2,vector<int>(limit+1,0)));
-
-        vector<vector<int>> curr(2,vector<int>(limit+1,0));
-        vector<vector<int>> next(2,vector<int>(limit+1,0));
-
-        for(int index = prices.size()-1;index>=0;index--)
-        {
-            for(int buy = 1;buy>=0;buy--)
-            {
-                for(int k=1;k<=limit;k++)
-                {
-                    int profit = 0;
-
-                    if(buy)
-                    {
-                        int take = -prices[index] + next[0][k];
-                        int notTake = next[1][k];
-
-                        profit = max(take,notTake);
-                    }
-                    else{
-                        int take = prices[index] + next[1][k-1];
-                        int notTake = next[0][k];
-
-                        profit = max(take,notTake);
-
-                    }
-
-                    curr[buy][k] = profit;
-                }
-            }
-
-            next=curr;
-        }
-        return next[1][limit];
-    }
     int maxProfit(int k, vector<int>& prices) {
-        
-        // vector<vector<vector<int> > > dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
-        // return solve(prices,0,1,k,dp);
+        // vector<vector<int>> dp(prices.size() + 1,vector<int>(2*k+1,0));
+        vector<int> curr(2*k+1,0);
+        vector<int> next(2*k+1,0);
 
-        return solveTab(k,prices);
+        for(int i = prices.size()-1;i>=0;i--)
+        {
+            for(int j = 2*k-1;j>=0;j--)
+            {
+                if((j & 1) == 0)
+                    curr[j] = max( -prices[i] + next[j+1], next[j]);
+                else
+                    curr[j] = max( prices[i] + next[j+1], next[j]);
+            }
+            next = curr;
+        }
+        return next[0];
     }
 };

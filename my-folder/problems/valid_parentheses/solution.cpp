028b8b61191ba928ab6a5f21855148a3733dd5f4
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-20 06:39:35 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- my-folder/problems/valid_parentheses/solution.cpp
+++ my-folder/problems/valid_parentheses/solution.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
+        stack<char> st;
 
-        stack<int> stk;
-
-        for(int i=0;i<s.size();i++)
+        for(auto it:s)
         {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
-                stk.push(s[i]);
-            
-            if(s[i] == ')')
-            {
-                if(stk.empty())
-                    return false;
-                if(stk.top() == '(')
-                    stk.pop();
+            if(it == '(' || it == '{' || it == '[')
+                st.push(it);
+            else{
+                char c;
+                if(!st.empty())
+                    c = st.top();
                 else
                     return false;
-            }
-            else if(s[i] == '}')
-            {
-                if(stk.empty())
-                    return false;
-                if(stk.top() == '{')
-                    stk.pop();
-                else
-                    return false;
-            }
-            else if(s[i] == ']')
-            {
-                if(stk.empty())
-                    return false;
-                if(stk.top() == '[')
-                    stk.pop();
+                if((it == ')' && c == '(') || (it == '}' && c == '{') || (it == ']' && c == '['))
+                    st.pop();
                 else
                     return false;
             }
         }
 
-        if(stk.empty())
-            return true;
-        return false;
+        return st.empty();
     }
 };

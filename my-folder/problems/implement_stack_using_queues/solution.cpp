147f78a9f8baf8a17931fd3bdd6763b99dd20d2d
Avnish Uniyal <97459731+its-coder5128@users.noreply.github.com>
2024-06-20 06:27:49 +0000

    Sync LeetCode submission - Implement Stack using Queues (cpp)

--- my-folder/problems/implement_stack_using_queues/solution.cpp
+++ my-folder/problems/implement_stack_using_queues/solution.cpp
@@ -1,37 +1,45 @@
 class MyStack {
-public:
     queue<int> q1;
     queue<int> q2;
+public:
+    MyStack() {
+        
+    }
     
     void push(int x) {
-
         q2.push(x);
-
         while(!q1.empty())
         {
             q2.push(q1.front());
             q1.pop();
         }
-
         swap(q1,q2);
     }
     
     int pop() {
-
-        int temp = q1.front();
-        q1.pop();
-        return temp;
-        
+        if(q1.empty())
+            return -1;
+        else
+        {
+            int x = q1.front();
+            q1.pop();
+            return x;
+        }
     }
     
     int top() {
-        return q1.front();
+        if(q1.empty())
+            return -1;
+        else
+        {
+            int x = q1.front();
+            return x;
+        }
+        
     }
     
     bool empty() {
-
         return q1.empty();
-        
     }
 };
 
